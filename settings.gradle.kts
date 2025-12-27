66d3ae75ed3d5c6da431a29b955e32ba3b7dafc1
zacky-dzacky <zackydzacky@gmail.com>
2025-12-27 20:32:52 +0700

    init app

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "local-genai-android"
+include(":app")
